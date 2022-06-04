fc09f209e95f5e4f1c106d46c3e024850eb81866
frgiovanna <giovannacnf@gmail.com>
2022-06-04 17:45:11 -0300

    add recursive functions

---
+++ recursion/collectOddValues.js
@@ -0,0 +1,20 @@
+// by a list of numbers, the function needs to return another list with the odd values
+
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+console.log(collectOddValues([1,2,3,4,5]))
+                                        
+                                                                
